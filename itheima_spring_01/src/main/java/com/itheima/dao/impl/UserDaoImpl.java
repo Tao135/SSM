62328a4cfdc6a2745394a7e34b3146778f3eccc7
Tao135 <1295072844@qq.com>
2022-03-12 15:38:30 +0800

    @Tao 测试bean中的init-method与destory-method属性 测试过程:在bean的全限定名称中，创建两个方法，对应创建和销毁时进行的操作，init-method对应创建时将执行的方法，destroy-method对应销毁时执行的方法 注意事项:bean实例为单例模式时，销毁由手动控制，close()；当bean实例为多例模式时，对象长时间不使用，会被Java的垃圾回收器回收

--- itheima_spring_01/src/main/java/com/itheima/dao/impl/UserDaoImpl.java
+++ itheima_spring_01/src/main/java/com/itheima/dao/impl/UserDaoImpl.java
@@ -4,6 +4,18 @@ import com.itheima.dao.UserDao;
 
 public class UserDaoImpl implements UserDao {
 
+    public UserDaoImpl(){
+        System.out.println("UserDaoImpl创建");
+    }
+
+    public void init(){
+        System.out.println("初始化...");
+    }
+
+   public void end(){
+       System.out.println("销毁...");
+   }
+
     public void save(){
         System.out.println("save running...");
     }
