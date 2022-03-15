4842b3a833f74d985c6c78aea7e8d16ffaf35f8e
Tao135 <1295072844@qq.com>
2022-03-15 20:28:19 +0800

    @Tao135 Spring集成Junit 操作步骤:导入spring集成Junit的坐标，在pom中导入maven；使用@Runwith注解替换原来的运行期；使用@ContextConfiguration指定配置文件或配置类;使用@Autowired注入需要测试的对象,需要测试什么就注入什么;创建测试方法进行测试。
    使用效果：可以省略每个测试方法要用到的以下两行代码
    ApplicationContext app = new ClassPathXmlApplicationContext("...");
    UserService userService = app.getBean(...);

--- itheima_spring_ioc_anno/src/test/java/com/itheima/config/SpringConfiguration.java
+++ itheima_spring_ioc_anno/src/test/java/com/itheima/config/SpringConfiguration.java
@@ -1,10 +1,7 @@
 package com.itheima.config;
 
-import com.mchange.v2.c3p0.ComboPooledDataSource;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 
-import javax.sql.DataSource;
 
 
 @Configuration      //标志此类是Spring的核心配置类
