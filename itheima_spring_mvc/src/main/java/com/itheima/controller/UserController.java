69b5eccab8ee45c31220331eb66a60bbd5959000
Tao135 <1295072844@qq.com>
2022-03-19 09:52:54 +0800

    @Tao135 SpringMVC组件 相关组件:前端控制器：DispatcherServlet          处理器映射器：HandlerMapping          处理器设配器：HandlerAdapter          处理器：Handler          视图解析器：View Resolver          视图：View 在Spring-mvc.xml中配置视图解析器，可以使controller跳转页面时，自动输入前缀和后缀 重点：重定向与转发，客户端发送请求时，转发一次，重定向两次，重定向会改变地址，转发是由服务器端转发，重定向是客户端转发

--- itheima_spring_mvc/src/main/java/com/itheima/controller/UserController.java
+++ itheima_spring_mvc/src/main/java/com/itheima/controller/UserController.java
@@ -1,15 +1,22 @@
 package com.itheima.controller;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @Controller
 public class UserController {
 
-    @RequestMapping("/quick")
+    @RequestMapping("/")
+    public String index(){
+        return "index.jsp";
+    }
+
+    @RequestMapping(value = "/quick",method = RequestMethod.GET,params = {"username"})
     public String save(){
         System.out.println("controller save running...");
-        return "/success.jsp";
+        return "success";
     }
 }
