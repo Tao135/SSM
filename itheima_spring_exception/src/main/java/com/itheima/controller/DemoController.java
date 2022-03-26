4d4d46483c676cc0084fc4f385f16265d1a95d10
Tao135 <1295072844@qq.com>
2022-03-26 23:02:08 +0800

    @Tao135 SpringMVC异常处理 适用作用：当程序报各种错误时，进行何种操作处理

---
+++ itheima_spring_exception/src/main/java/com/itheima/controller/DemoController.java
@@ -0,0 +1,25 @@
+package com.itheima.controller;
+
+import com.itheima.service.DemoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class DemoController {
+
+    @Autowired
+    private DemoService demoService;
+
+    @RequestMapping("/show")
+    public String show() throws Exception{
+        System.out.println("show running...");
+        //demoService.show1();
+        //demoService.show2();
+        //demoService.show3();
+        demoService.show4();
+        //demoService.show5();
+        return "index";
+    }
+}
