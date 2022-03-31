311b8c6e649bbb58deecb754fbfa3d195ad483d6
Tao135 <1295072844@qq.com>
2022-03-31 21:10:04 +0800

    @Tao135 Mybatis增删改查操作 注意事项：输入的与输出的类型，应该与Domain层对应

--- itheima_mybatis_quick/src/test/java/MyBatisTest.java
+++ itheima_mybatis_quick/src/test/java/MyBatisTest.java
@@ -13,14 +13,33 @@ public class MyBatisTest {
 
     @Test
     public void test1() throws Exception{
+
+        //模拟User对象
+        User user = new User();
+        user.setUsername("tom");
+        user.setPassword("123");
+
+
         //获得核心配置文件
         InputStream resourceAsStream = Resources.getResourceAsStream("sqlMapConfig.xml");
         //获得session工厂对象
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
         //获得session回话对象
         SqlSession sqlSession = sqlSessionFactory.openSession();
-        //执行操作 参数:namespace+id
+        //执行查询操作 参数:namespace+id
         List<User> userList = sqlSession.selectList("userMapper.findAll");
+
+        //更新操作
+        sqlSession.update("userMapper.update",user);
+
+
+        //删除操作
+        sqlSession.delete("userMapper.delete",2);
+
+        //插入操作，并且提交事务
+        sqlSession.insert("userMapper.save",user);
+        sqlSession.commit();
+
         //打印数据
         System.out.println(userList);
 
