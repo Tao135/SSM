8ba4fc3d3b57c1308a3c462800dc62a3096f138f
Tao135 <1295072844@qq.com>
2022-04-03 19:11:55 +0800

    @Tao135 Mybatis的Dao层实现 注意事项：1.mapper文件的namespace是接口的全限类名；2.方法的名称与id一致；3.方法返回值与sql语句返回值一致；4.方法的形参与sql语句形参一致。

---
+++ itheima_mybatis_dao/src/main/java/com/itheima/dao/UserMapper.java
@@ -0,0 +1,12 @@
+package com.itheima.dao;
+
+import com.itheima.domain.User;
+
+import java.util.List;
+
+public interface UserMapper {
+
+    public List<User> findAll() throws Exception;
+
+    public User findById(int id);
+}
---
+++ itheima_mybatis_dao/src/main/java/com/itheima/service/ServiceDemo.java
@@ -0,0 +1,27 @@
+package com.itheima.service;
+
+import com.itheima.dao.UserMapper;
+import com.itheima.domain.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.InputStream;
+import java.util.List;
+
+public class ServiceDemo {
+
+    public static void main(String[] args) throws Exception {
+        InputStream resourceAsStream = Resources.getResourceAsStream("sqlMapConfig.xml");
+        SqlSessionFactory sqlsessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        SqlSession sqlSession = sqlsessionFactory.openSession();
+
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        List<User> userList = mapper.findAll();
+        System.out.println(userList);
+
+        User byId = mapper.findById(1);
+        System.out.println(byId);
+    }
+}
