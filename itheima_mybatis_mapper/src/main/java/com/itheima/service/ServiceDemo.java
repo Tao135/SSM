ac4092d048ef5186e421696aec3591083782cd1a
Tao135 <1295072844@qq.com>
2022-04-03 23:43:17 +0800

    @Tao135 MyBatis映射文件 相关内容：<select><insert><update><delete>等sql语句，在sql语句中可以使用<where> <if>进行内容判断动态变化sql语句，以及使用<foreach>可以在不缺定有多少个时使用。

---
+++ itheima_mybatis_mapper/src/main/java/com/itheima/service/ServiceDemo.java
@@ -0,0 +1,31 @@
+package com.itheima.service;
+
+import com.itheima.dao.UserMapper;
+import com.itheima.domain.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServiceDemo {
+
+    public static void main(String[] args) throws Exception {
+        InputStream resourceAsStream = Resources.getResourceAsStream("sqlMapConfig.xml");
+        SqlSessionFactory sqlsessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        SqlSession sqlSession = sqlsessionFactory.openSession();
+
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+
+        //模拟ids的数据
+        List<Integer> ids = new ArrayList<Integer>();
+        ids.add(1);
+
+        List<User> userList = mapper.findByIds(ids);
+        System.out.println(userList);
+
+    }
+}
